fdcf61412836c4150f1a96890396ef9a44d085c8
ceneri <ceneri@ucsc.edu>
2017-12-01 15:01:18 -0800

    Next/Prev

--- app/src/main/java/com/funnums/funnums/classes/PopUp.java
+++ app/src/main/java/com/funnums/funnums/classes/PopUp.java
@@ -7,6 +7,7 @@ import android.text.Layout;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import com.funnums.funnums.R;
@@ -20,11 +21,13 @@ import com.funnums.funnums.R;
 public class PopUp extends Activity {
     static final public String TAG = "PopUp";
 
+    final private double POPUP_XSCREEN_PERCENT_USED = 0.80;   /*80%*/
+    final private double POPUP_YSCREEN_PERCENT_USED = 0.80;   /*80%*/
+
     private int screenX;
     private int screenY;
-
-    private double POPUP_XSCREEN_PERCENT_USED = 0.80;   /*80%*/
-    private double POPUP_YSCREEN_PERCENT_USED = 0.90;   /*75%*/
+    private String game_type;
+    private RelativeLayout rl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -35,8 +38,8 @@ public class PopUp extends Activity {
 
         //Set proper image
         Bundle extras = getIntent().getExtras();
-        String game_type = extras.getString("minigame");
-        RelativeLayout rl = (RelativeLayout) findViewById(R.id.pop_up_window);
+        game_type = extras.getString("minigame");
+        rl = (RelativeLayout) findViewById(R.id.pop_up_window);
 
         switch (game_type){
             case("bubble"):
@@ -61,5 +64,30 @@ public class PopUp extends Activity {
                 (int) (screenX* POPUP_XSCREEN_PERCENT_USED),
                 (int) (screenY * POPUP_YSCREEN_PERCENT_USED)
         );
+
+        Log.d(TAG, "PopUp window created");
     }
+
+    public void onPressedNextTip(View v) {
+        Log.d(TAG, "Next Tip Pressed");
+
+        //TODO change to proper image and keep record if its the last image
+        rl.setBackgroundResource(R.drawable.owl_popup2);
+
+        //Set previous button to visible if it not currently visible
+        Button previous_button = (Button) findViewById(R.id.pop_up_button_p);
+        if (previous_button.getVisibility() == View.INVISIBLE){
+            previous_button.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    public void onPressedPreviousTip(View v) {
+        Log.d(TAG, "Previous Tip Pressed");
+        rl.setBackgroundResource(R.drawable.owl_popup);
+        Button b = (Button) findViewById(R.id.pop_up_button_p);
+        b.setVisibility(View.INVISIBLE);
+    }
+
+
 }
